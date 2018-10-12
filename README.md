b845d1ff845ddc3e957210d1b19e11b87bfc4d18
Maxence Bouret <maxence.bouret@gmail.com>
2018-10-12 17:06:00 +0200

    Update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# w1-friday-dom-manipulation
+# w1-friday-dom-manipulation
 
-## Screenshot
+## Screenshot
 ![image](https://user-images.githubusercontent.com/5306791/46875082-107fbf80-ce3b-11e8-8893-cc4b25e42a65.png)
--- main.js
+++ main.js
@@ -14,7 +14,24 @@ document.getElementsByClassName('ta')[1].style.color = "yellow";
 // Tor
 document.querySelectorAll('ul .ta')[2].style.color = "purple";
 
+// Silvio
+var $li = document.createElement('li')
+$li.innerText = "Silvio the Snake"
+document.querySelector('ul').appendChild($li)
+$li.style.color = "green"
+
 var $tas = document.querySelectorAll('ul .ta')
 for (var i = 0; i < $tas.length; i++) {
   $tas[i].style.backgroundColor = "orange"
+}
+
+// Changing the HREF tag
+var $link = document.getElementsByTagName('a')[0]
+$link.setAttribute('href', 'https://www.google.de/search?q=monkey&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiHt-jskoHeAhWPKlAKHQ05C7YQ_AUIDigB&biw=447&bih=572&dpr=1.75')
+$link.setAttribute('target', 'blank')
+
+// Button
+var $button = document.getElementById('add-animal')
+$button.onclick = function() {
+  console.log("You clicked on the button!!")
 }
